96cf473c49eb3b57692375854f381217968b6bd6
Lev <yarken121@gmail.com>
2024-01-26 16:13:18 +0500

    Created empty react app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+import React from 'react';
+
+const App = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default App
